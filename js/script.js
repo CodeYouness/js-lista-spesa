d44fc5886a6c162b9f6af6f9a7027b5bb2e5e5d0
CodeYouness <ylijassi@gmail.com>
2024-05-15 15:45:10 +0200

    added array with element added li element added ul element

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const listItem = ['pomodorini', 'patate', 'cipolle', 'carciofi']
+const liEl = document.createElement('li')
+const ulEl = document.querySelector('ul')
+
+console.log(liEl, listItem, ulEl)
