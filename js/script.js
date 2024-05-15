de6c4c376951c98a3354dd140aa376550dc416e5
CodeYouness <ylijassi@gmail.com>
2024-05-15 15:53:25 +0200

    fixed problem that print just the last element of the array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const listItem = ['pomodorini', 'patate', 'cipolle', 'carciofi'];
-const liEl = document.createElement('li');
 const ulEl = document.querySelector('ul');
 let i = 0;
 
 while (i < listItem.length) {
+    const liEl = document.createElement('li');
     liEl.innerHTML = listItem[i]
     ulEl.appendChild(liEl)
     i += 1
