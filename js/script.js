1b8a696e4c1498f2e697984f14a8c3d2cbd8e5a5
CodeYouness <ylijassi@gmail.com>
2024-05-15 15:51:40 +0200

    added while cycle that stops at the listitem.length

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-const listItem = ['pomodorini', 'patate', 'cipolle', 'carciofi']
-const liEl = document.createElement('li')
-const ulEl = document.querySelector('ul')
+const listItem = ['pomodorini', 'patate', 'cipolle', 'carciofi'];
+const liEl = document.createElement('li');
+const ulEl = document.querySelector('ul');
+let i = 0;
 
-console.log(liEl, listItem, ulEl)
+while (i < listItem.length) {
+    liEl.innerHTML = listItem[i]
+    ulEl.appendChild(liEl)
+    i += 1
+    console.log(liEl, listItem, ulEl, i)
+}
+
